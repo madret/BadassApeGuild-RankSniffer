6bfd3366fa2e286ecd9552b5c5a65f0c0afc183a
b41s <56820649+madret@users.noreply.github.com>
2022-01-30 16:31:49 +0100

    Update bag_rank_sniffer.py

--- ranksniffer/bag_rank_sniffer.py
+++ ranksniffer/bag_rank_sniffer.py
@@ -15,8 +15,8 @@ driver = webdriver.Chrome(options=options,service=s)
 #BUSD search filter
 driver.get('https://www.binance.com/en/nft/collection/Badass-Ape-Guild?currency=BUSD&amountFrom=800&amountTo=3000&orderBy=amount_sort&orderType=1&isBack=1&id=520838987141468161&order=amount_sort%401')
 #Get pass the Agreement & cookie wall:
-WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[3]/div/div[1]/div/div[2]/div/button[2]"))).click()
-WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Accept']"))).click()
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[3]/div/div[1]/div/div[2]/div/button[2]"))).click()
+WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Accept']"))).click()
 
 print ("""
   ____            _                     _                   ____       _ _     _ 
